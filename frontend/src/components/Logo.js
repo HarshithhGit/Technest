351b74358d76703968ce5ed00e23397f87733871
HARSHITH.H <harshith.h0308@gmail.com>
2026-07-18 00:44:00 +0530

    Update Logo.js

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -7,14 +7,11 @@ export default function Logo({ className = "" }) {
       className={`flex items-center justify-center w-14 h-14 rounded-full overflow-hidden border-2 border-blue-500 shadow-lg bg-black cursor-pointer -mt-5
          ${className}`}
     >
-      <Image
-        src="/logo.jpeg"
-        alt="TechNest Projects"
-        width={56}
-        height={56}
-        priority
-        className="w-full h-full object-cover rounded-full"
-      />
+      <img
+  src="/logo.jpeg"
+  alt="TechNest Projects"
+  className="w-full h-full object-cover rounded-full"
+/>
     </div>
   );
 }
